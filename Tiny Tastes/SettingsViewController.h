7bf9aaac00ee75178a6d5e191a2c8bed159c1c48
nsuaifan <nsuaifan@gmail.com>
2013-09-18 21:34:44 -0400

    added settings screen

---
+++ Tiny Tastes/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  Tiny Tastes
+//
+//  Created by Noelle Suaifan on 9/18/13.
+//  Copyright (c) 2013 Le Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
