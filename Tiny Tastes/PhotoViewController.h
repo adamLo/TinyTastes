96c15b65e5e1e89c7d8b30697a0167ba2f370878
guest user <guestuser@wl-10-190-203-153.wireless.duke.edu>
2013-10-08 19:43:51 -0400

    Added Photo View Controller

---
+++ Tiny Tastes/PhotoViewController.h
@@ -0,0 +1,15 @@
+//
+//  PhotoViewController.h
+//  Tiny Tastes
+//
+//  Created by guest user on 10/8/13.
+//  Copyright (c) 2013 Le Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoViewController : UIViewController<UIImagePickerControllerDelegate,UINavigationControllerDelegate>
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+- (IBAction)takePhoto:(UIButton *)sender;
+
+@end
---
+++ Tiny Tastes/PhotoViewController.m
@@ -0,0 +1,78 @@
+//
+//  PhotoViewController.m
+//  Tiny Tastes
+//
+//  Created by Jei Min Yoo on 10/8/13.
+//  Reference: http://www.appcoda.com/ios-programming-camera-iphone-app/
+//  Copyright (c) 2013 Le Labs. All rights reserved.
+//
+
+#import "PhotoViewController.h"
+
+@interface PhotoViewController ()
+
+@end
+
+@implementation PhotoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    
+    // for iOS simulator - if device doesn't have a camera, display an error message
+    if (![UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        
+        UIAlertView *myAlertView = [[UIAlertView alloc] initWithTitle:@"Error"
+                                                              message:@"Device has no camera"
+                                                             delegate:nil
+                                                    cancelButtonTitle:@"OK"
+                                                    otherButtonTitles: nil];
+        
+        [myAlertView show];
+        
+    }
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)takePhoto:(UIButton *)sender {
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.delegate = self;
+    picker.allowsEditing = YES;
+    picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+    
+    [self presentViewController:picker animated:YES completion:NULL];
+    
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    
+    UIImage *chosenImage = info[UIImagePickerControllerEditedImage];
+    self.imageView.image = chosenImage;
+    
+    [picker dismissViewControllerAnimated:YES completion:NULL];
+    
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    
+    [picker dismissViewControllerAnimated:YES completion:NULL];
+    
+}
+
+@end
