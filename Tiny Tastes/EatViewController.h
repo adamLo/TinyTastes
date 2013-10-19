ebe3c3c1401b0695d12ba414957ac187ed6bd645
nsuaifan <nsuaifan@gmail.com>
2013-10-19 18:10:30 -0400

    Modified Let's Eat Screen a lot

--- Tiny Tastes/EatViewController.h
+++ Tiny Tastes/EatViewController.h
@@ -8,6 +8,21 @@
 
 #import <UIKit/UIKit.h>
 
-@interface EatViewController : UIViewController
+@interface EatViewController : UIViewController {
+    IBOutlet UILabel *countdownLabel;
+    NSTimer *countdownTimer;
+    int secondsCount;
+    IBOutlet UIImageView *eatingCritter;
+    
+    IBOutlet UIImageView *doneButtonImage;
+    IBOutlet UIImageView *allFinishedButtonImage;
+    IBOutlet UIImageView *partiallyFinishedButtonImage;
+    IBOutlet UIImageView *notFinishedButtonImage;
+    
+    IBOutlet UIButton *doneButton;
+    IBOutlet UIButton *allFinishedButton;
+    IBOutlet UIButton *partiallyFinishedButton;
+    IBOutlet UIButton *notFinishedButton;
+}
 
 @end
--- Tiny Tastes/EatViewController.m
+++ Tiny Tastes/EatViewController.m
@@ -26,14 +26,59 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
-        self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"frame.jpg"]];
+    allFinishedButton.hidden = YES;
+    allFinishedButtonImage.alpha = 0;
+    partiallyFinishedButton.hidden = YES;
+    partiallyFinishedButtonImage.alpha = 0;
+    notFinishedButton.hidden = YES;
+    notFinishedButtonImage.alpha = 0;
+    
+    //self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"frame.jpg"]];
+ 
+    eatingCritter.animationImages = [NSArray arrayWithObjects:[UIImage imageNamed:@"eating_critter_1.jpg"],
+                                     [UIImage imageNamed:@"eating_critter_2.jpg"], nil];
+    eatingCritter.animationDuration = 1;
+    [self.view addSubview:eatingCritter];
+    [eatingCritter startAnimating];
+    
+    [self setTimer];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)timerRun {
+    secondsCount = secondsCount - 1;
+    int minutes = secondsCount / 60;
+    int seconds = secondsCount - (minutes * 60);
+    
+    NSString *timerOutput = [NSString stringWithFormat:@"%2d:%.2d", minutes, seconds];
+    countdownLabel.text = timerOutput;
+    
+    if (secondsCount == 0) {
+        [countdownTimer invalidate];
+        countdownTimer = nil;
+        [self doneButtonClicked];
+    }
+    
+}
+- (void)setTimer {
+    secondsCount = 100;
+    countdownTimer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(timerRun) userInfo:nil repeats: YES];
+}
+
+- (IBAction)doneButtonClicked {
+    allFinishedButton.hidden = NO;
+    partiallyFinishedButton.hidden = NO;
+    notFinishedButton.hidden = NO;
+    doneButton.hidden = 0;
+    allFinishedButtonImage.alpha = 1;
+    partiallyFinishedButtonImage.alpha = 1;
+    notFinishedButtonImage.alpha = 1;
+    doneButtonImage.alpha = 0;
 }
 
 @end
