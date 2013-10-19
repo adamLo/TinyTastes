c0c6571861100d40191d61a2110c2a1e2bd29bcc
nsuaifan <nsuaifan@gmail.com>
2013-10-19 14:41:28 -0400

    reformatted home screen

--- Tiny Tastes/MealNotificationViewController.h
+++ Tiny Tastes/MealNotificationViewController.h
@@ -8,6 +8,5 @@
 
 #import <UIKit/UIKit.h>
 
-@interface MealNotificationViewController : UITableViewController
-
+@interface MealNotificationViewController : UITableViewController <UITableViewDelegate, UITableViewDataSource>
 @end
