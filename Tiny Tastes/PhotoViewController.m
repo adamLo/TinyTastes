7f921cab3c0a6c73878d392801bdc05ad65db510
guest user <guestuser@empire09.local>
2013-11-03 21:03:47 -0500

    camera API - cropping oval shaped image from picture

--- Tiny Tastes/PhotoViewController.m
+++ Tiny Tastes/PhotoViewController.m
@@ -5,12 +5,14 @@
 //  Created by Jei Min Yoo on 10/8/13.
 //  Reference: http://www.appcoda.com/ios-programming-camera-iphone-app/
 //  http://www.pushplay.net/2010/03/one-tap-uiimagepickercontroller-iphone-camera/
+//  Cropping & Masking: http://iosdevelopertips.com/cocoa/how-to-mask-an-image.html#comment-58842
 //  Copyright (c) 2013 Le Labs. All rights reserved.
 //
 
 #import <AVFoundation/AVFoundation.h>
 #import "PhotoViewController.h"
 #import "OverlayView.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface PhotoViewController ()
 
@@ -77,18 +79,12 @@
 
 - (void) imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
     
-    if (info == NULL) {
-        NSLog(@"info null");
-    }
-    
-    if (info[UIImagePickerControllerEditedImage] == NULL) {
-        NSLog(@"image null");
-    }
-        
         UIImage *chosenImage = info[UIImagePickerControllerEditedImage];
-        self.imageView.image = chosenImage;
         self.cameraIcon.hidden = YES;
         self.instructionLabel.hidden = YES;
+    
+        // Crop the image with an image mask
+        self.imageView.image = [self maskImage :chosenImage withMask:[UIImage imageNamed:@"cutout.jpg"]];
         
         [picker dismissViewControllerAnimated:YES completion:NULL];
 
@@ -98,6 +94,61 @@
     
     [picker dismissViewControllerAnimated:YES completion:NULL];
     
+}
+
+-(CGImageRef) CopyImageAndAddAlphaChannel :(CGImageRef) sourceImage
+{
+    CGImageRef retVal = NULL;
+    
+    size_t width = CGImageGetWidth(sourceImage);
+    size_t height = CGImageGetHeight(sourceImage);
+    
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    
+    CGContextRef offscreenContext = CGBitmapContextCreate(NULL, width, height,
+                                                          8, 0, colorSpace, kCGImageAlphaPremultipliedFirst);
+    
+    if (offscreenContext != NULL) {
+        CGContextDrawImage(offscreenContext, CGRectMake(0, 0, width, height), sourceImage);
+        
+        retVal = CGBitmapContextCreateImage(offscreenContext);
+        CGContextRelease(offscreenContext);
+    }
+    
+    CGColorSpaceRelease(colorSpace);
+    
+    return retVal;
+}
+
+- (UIImage*)maskImage:(UIImage *)image withMask:(UIImage *)maskImage {
+    CGImageRef maskRef = maskImage.CGImage;
+    CGImageRef mask = CGImageMaskCreate(CGImageGetWidth(maskRef),
+                                        CGImageGetHeight(maskRef),
+                                        CGImageGetBitsPerComponent(maskRef),
+                                        CGImageGetBitsPerPixel(maskRef),
+                                        CGImageGetBytesPerRow(maskRef),
+                                        CGImageGetDataProvider(maskRef), NULL, false);
+    
+    CGImageRef sourceImage = [image CGImage];
+    CGImageRef imageWithAlpha = sourceImage;
+    //add alpha channel for images that don’t have one (ie GIF, JPEG, etc…)
+    //this however has a computational cost
+    if (CGImageGetAlphaInfo(sourceImage) == kCGImageAlphaNone) {
+        imageWithAlpha = [self CopyImageAndAddAlphaChannel :sourceImage];
+    }
+    
+    CGImageRef masked = CGImageCreateWithMask(imageWithAlpha, mask);
+    CGImageRelease(mask);
+    
+    //release imageWithAlpha if it was created by CopyImageAndAddAlphaChannel
+    if (sourceImage != imageWithAlpha) {
+        CGImageRelease(imageWithAlpha);
+    }
+    
+    UIImage* retImage = [UIImage imageWithCGImage:masked];
+    CGImageRelease(masked);
+    
+    return retImage;
 }
 
 @end
